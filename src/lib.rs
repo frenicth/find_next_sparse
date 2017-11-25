79ae1aa82717f95a82861fcaeac0e302ca05909f
Fredrik Nilsson <frednils@student.chalmers.se>
2017-11-25 13:08:18 +0100

    edited the wrong file in previous commits...

--- src/lib.rs
+++ src/lib.rs
@@ -1,69 +1,80 @@
-fn next_sparse_num(mut num: u32) -> u32 {
-    let mut next = true;
-    let mut shifts;
-    let mut add;
-    while next == true {
 
-        let tuple = is_sparse(num);
-        next = tuple.0;
-        shifts = tuple.1;
+/// This is a small library for finding sparse numbers.
+/// A number is sparse if there are no consecutive ones
+/// in its binary representation.
+///
+pub mod sparse {
 
-        if next == true {
-            add = 1;
-            add <<= shifts - 1;
-            num = num + add;
-        } else {
-            return num;
-        }
-    }
-    return 0;
-}
+    //! Returns the next sparse number after
+    //! @param num. If num is sparse, num will
+    //! be returned.
+    pub fn next_sparse_num(mut num: u32) -> u32 {
+        let mut next = true;
+        let mut shifts;
+        let mut add;
+        while next == true {
 
-fn is_sparse(mut num: u32) -> (bool, u32) {
-    let mut next = false;
-    let mut sequential_ones = 0;
-    let mut shifts = 0;
+            let tuple = is_sparse(num);
+            next = tuple.0;
+            shifts = tuple.1;
 
-    while num > 0 {
-        if num & 1 == 1 {
-            sequential_ones = sequential_ones + 1;
-            if sequential_ones == 2 {
-                next = true;
-                break;
+            if next == true {
+                add = 1;
+                add <<= shifts - 1;
+                num = num + add;
+            } else {
+                return num;
             }
-        } else {
-            sequential_ones = 0;
         }
-        shifts = shifts + 1;
-        num >>= 1;
+        return 0;
     }
-    if next == true {
-        return (true, shifts);
-    } else {
-        return (false, num);
+
+    fn is_sparse(mut num: u32) -> (bool, u32) {
+        let mut next = false;
+        let mut sequential_ones = 0;
+        let mut shifts = 0;
+
+        while num > 0 {
+            if num & 1 == 1 {
+                sequential_ones = sequential_ones + 1;
+                if sequential_ones == 2 {
+                    next = true;
+                    break;
+                }
+            } else {
+                sequential_ones = 0;
+            }
+            shifts = shifts + 1;
+            num >>= 1;
+        }
+        if next == true {
+            return (true, shifts);
+        } else {
+            return (false, num);
+        }
     }
 }
 
 #[test]
 fn test_next_sparse_num_4() {
-    let val = next_sparse_num(4);
+    let val = sparse::next_sparse_num(4);
     assert_eq!(val, 4, "{} != {}", val, 4);
 }
 
 #[test]
 fn test_next_sparse_num_8() {
-    let val = next_sparse_num(6);
+    let val = sparse::next_sparse_num(6);
     assert_eq!(val, 8, "{} != {}", val, 8);
 }
 
 #[test]
 fn test_next_sparse_num_40() {
-    let val = next_sparse_num(38);
+    let val = sparse::next_sparse_num(38);
     assert_eq!(val, 40, "{} != {}", val, 40);
 }
 
 #[test]
 fn test_next_sparse_num_64() {
-    let val = next_sparse_num(44);
+    let val = sparse::next_sparse_num(44);
     assert_eq!(val, 64, "{} != {}", val, 64);
 }
